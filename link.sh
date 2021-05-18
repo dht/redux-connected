87e80da1d784378026f8fe9acd8eb1090d69649a
dht <guyguys@gmail.com>
2021-05-18 13:34:49 +0300

    wip

--- link.sh
+++ link.sh
@@ -3,19 +3,21 @@ cd packages
 cd redux-connected
 yarn unlink
 yarn link
+yarn link redux-store-generator
 
 cd ../redux-connected-devtools
 yarn unlink
 yarn link
-yarn link redux-store-connected
+yarn link redux-connected
 
 cd ../redux-connected-components
 yarn unlink
 yarn link
-yarn link redux-store-connected
+yarn link redux-store-generator
+yarn link redux-connected
 
 cd ../redux-connected-examples/client
 yarn link redux-connected
-yarn link redux-devtools
-yarn link redux-components
+yarn link redux-connected-devtools
+yarn link redux-connected-components
 
