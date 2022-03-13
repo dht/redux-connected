795709a4ce4cdcf9fbed992830dccc9f8a175066
dht <dht@users.noreply.github.com>
2022-03-14 00:05:23 +0200

    wip

--- src/store/selectors.ts
+++ src/store/selectors.ts
@@ -1,38 +1,52 @@
 import { createSelector } from 'reselect';
-import { ConnectedStore, RequestStatus } from '../types';
+import {
+    ApiRequest,
+    ApiRequests,
+    ConnectedStore,
+    RequestStatus,
+} from '../types';
 
 export const $i = (i: ConnectedStore) => i;
 
-export const $actionTypesRaw = createSelector($i, (state) => state.actionTypes); // prettier-ignore
-export const $apiGlobalSettingsRaw = createSelector($i, (state) => state.apiGlobalSettings); // prettier-ignore
-export const $apiGlobalStatsRaw = createSelector($i, (state) => state.apiGlobalStats); // prettier-ignore
-export const $endpointsConfigRaw = createSelector($i, (state) => state.endpointsConfig); // prettier-ignore
-export const $endpointsStateRaw = createSelector($i, (state) => state.endpointsState); // prettier-ignore
-export const $nodeTypesRaw = createSelector($i, (state) => state.nodeTypes); // prettier-ignore
-export const $requestsRaw = createSelector($i, (state) => state.requests); // prettier-ignore
-export const $lastActionRaw = createSelector($i, (state) => state._lastAction); // prettier-ignore
+export const $actionTypesRaw = createSelector($i, (state: ConnectedStore) => state.actionTypes); // prettier-ignore
+export const $apiGlobalSettingsRaw = createSelector($i, (state: ConnectedStore) => state.apiGlobalSettings); // prettier-ignore
+export const $apiGlobalStatsRaw = createSelector($i, (state: ConnectedStore) => state.apiGlobalStats); // prettier-ignore
+export const $endpointsConfigRaw = createSelector($i, (state: ConnectedStore) => state.endpointsConfig); // prettier-ignore
+export const $endpointsStateRaw = createSelector($i, (state: ConnectedStore) => state.endpointsState); // prettier-ignore
+export const $nodeTypesRaw = createSelector($i, (state: ConnectedStore) => state.nodeTypes); // prettier-ignore
+export const $requestsRaw = createSelector($i, (state: ConnectedStore) => state.requests); // prettier-ignore
+export const $lastActionRaw = createSelector($i, (state: ConnectedStore) => state._lastAction); // prettier-ignore
 
-export const $requests = createSelector($requestsRaw, (requests) => {
-    return Object.values(requests).sort((a, b) => {
-        if (a.createdTS === b.createdTS) {
-            return 0;
-        }
+export const $requests = createSelector(
+    $requestsRaw,
+    (requests: ApiRequests) => {
+        return Object.values(requests).sort((a, b) => {
+            if (a.createdTS === b.createdTS) {
+                return 0;
+            }
 
-        return a.createdTS > b.createdTS ? 1 : -1;
-    });
-});
+            return a.createdTS > b.createdTS ? 1 : -1;
+        });
+    }
+);
 
-export const $requestsNew = createSelector($requests, (requests) => {
-    return requests.filter((request) => {
-        request.requestStatus === RequestStatus.CREATED;
-    });
-});
+export const $requestsNew = createSelector(
+    $requests,
+    (requests: ApiRequest[]) => {
+        return requests.filter((request) => {
+            request.requestStatus === RequestStatus.CREATED;
+        });
+    }
+);
 
-export const $requestsQueued = createSelector($requests, (requests) => {
-    return requests.filter((request) => {
-        request.requestStatus === RequestStatus.IN_QUEUE;
-    });
-});
+export const $requestsQueued = createSelector(
+    $requests,
+    (requests: ApiRequest[]) => {
+        return requests.filter((request) => {
+            request.requestStatus === RequestStatus.IN_QUEUE;
+        });
+    }
+);
 
 export const connectedSelectors = {
     $actionTypesRaw,
